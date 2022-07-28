b6635a742f1d3722d0f794a4a4a489c3eb7cd676
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-07-28 22:07:22 +0600

    🚀 Project Initialized

---
+++ src/main/java/com/ewallet/nidapi/NidApiApplication.java
@@ -0,0 +1,13 @@
+package com.ewallet.nidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NidApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NidApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ewallet/nidapi/NidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewallet.nidapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NidApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
