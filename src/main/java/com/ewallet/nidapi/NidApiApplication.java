e065deefc15fc57aa5f58ec3b33f1499819dc98b
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-09-14 14:18:30 +0600

    ✅ Added Properties file for secrets

--- src/main/java/com/ewallet/nidapi/NidApiApplication.java
+++ src/main/java/com/ewallet/nidapi/NidApiApplication.java
@@ -1,25 +1,22 @@
 package com.ewallet.nidapi;
 
+
 import com.ewallet.nidapi.initialize.InitializeData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class NidApiApplication implements CommandLineRunner {
-
-    @Autowired
-    private InitializeData initializeData;
+public class NidApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NidApiApplication.class, args);
     }
 
 
-    @Override
-    public void run(String... args) throws Exception {
-        initializeData.initialize();
+   @Bean
+    CommandLineRunner runner(InitializeData initializeData) {
+        return args -> initializeData.initialize();
     }
 }
